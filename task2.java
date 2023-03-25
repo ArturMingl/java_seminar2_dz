b9eebbdf6984e074a5c4cca87273b416ea7a20d7
ArturMingl <adiscov90@gmail.com>
2023-03-25 11:18:54 +0300

    task2 done

---
+++ task2.java
@@ -0,0 +1,36 @@
+package java_tasks.Seminar2dz;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**Дана строка (получение через обычный текстовый файл!!!)
+ * "фамилия":"Иванов","оценка":"5","предмет":"Математика"
+"фамилия":"Петрова","оценка":"4","предмет":"Информатика"
+
+Написать метод(ы), который распарсит строку и, используя StringBuilder, создаст строки вида:
+Студент [фамилия] получил [оценка] по предмету [предмет].
+ */
+public class task2 {
+
+    public static void main(String[] args) {
+        String dataFile = "data_file_task2.txt";
+        String[] infoBase;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(dataFile));
+            String line; {
+                while ((line = br.readLine()) != null) {
+                    infoBase = line.replace("\"", "").split(":|,");
+                    sb.append("Студент ").append(infoBase[1]).append(" получил ").append(infoBase[3])
+                            .append(" по предмету ").append(infoBase[5]).append(".\n");
+                }
+                System.out.println(sb);
+            }
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
